35fba7ceefdee16e1f285edfd222cf9f5ff7023e
Aditya <adityakumarcs15@gmail.com>
2024-01-19 13:12:16 +0530

    added user and their roles

--- src/main/java/com/aadi/MvcSecurity/security/SecurityConfig.java
+++ src/main/java/com/aadi/MvcSecurity/security/SecurityConfig.java
@@ -1,7 +1,40 @@
 package com.aadi.MvcSecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
+
+    /**
+     * The function define the User and their Roles which will be part of Application run time.
+     *
+     * @return
+     */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        final UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test1")
+                .roles("EMPLOYEE")
+                .build();
+
+        final UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test12")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        final UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
 }
